950d9d7c6c199e49aafbf096962183655a7db0d4
K-VENKATA-KARTHIK-19 <venkatkarthikumar@gmail.com>
2025-05-21 22:15:56 +0530

    add angular and dotnet projects in separate folders

---
+++ dotnet/AngularAuthKAPI/AngularAuthKAPI/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AngularAuthKAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace AngularAuthKAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<UserDetails> UsersDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<UserDetails>().ToTable("UsersDetails");
+        }
+    }
+}
